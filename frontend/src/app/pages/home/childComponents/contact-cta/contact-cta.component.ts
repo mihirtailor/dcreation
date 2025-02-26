6f451ccc1712585280e2c75a8877d536b48b2cae
Mihir Tailor <mihirtailor1994@gmail.com>
2025-02-26 18:49:48 -0500

    update home contact cta component

--- frontend/src/app/pages/home/childComponents/contact-cta/contact-cta.component.ts
+++ frontend/src/app/pages/home/childComponents/contact-cta/contact-cta.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact-cta',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './contact-cta.component.html',
   styleUrl: './contact-cta.component.scss'
 })
 export class ContactCtaComponent {
+  window = window;
+
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
 
 }
